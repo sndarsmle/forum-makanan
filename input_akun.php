2cd42336d052ba65fa2a46c088e7980093b60b4a
ageng <rizsanaa23@gmail.com>
2023-11-29 09:49:03 +0700

    first

---
+++ input_akun.php
@@ -0,0 +1,31 @@
+<?php
+include "koneksiAkun.php";
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+$rePassword = $_POST['rePassword'];
+
+
+
+// Validasi kesamaan password
+if ($password != $rePassword) {
+    header('location:daftar.php?pesan=gagalInput');
+    exit;
+}
+
+
+$query = "INSERT INTO user (username, password) VALUES ('$username', '$password')";
+
+
+$result = mysqli_query($konek, $query);
+
+if ($result) {
+    header("Location: login.php");
+    exit;
+} else {
+    echo "Proses Input gagal";
+}
+
+// Tutup koneksi
+mysqli_close($konek);
+?>
