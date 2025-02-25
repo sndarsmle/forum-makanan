9f8b7b3f24571758353d8b135e1c470cef647490
Ageng Sandar <126995831+sndarsmle@users.noreply.github.com>
2025-02-25 17:27:35 +0700

    Delete notifikasi directory

--- notifikasi/notifPassword.php
+++
@@ -1,4 +0,0 @@
-<div class="alert alert-warning alert-dismissible fade show" role="alert">
-   <strong>Ada kesalahan!</strong> Password yang anda masukan tidak sama.
-   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
- </div>
